db3b498885c717810f66ef1821aa251c04ee8965
Marco DE BOOIJ <marco.developer@debooy.eu>
2021-02-20 23:03:28 +0100

    Testen van CSV invoer.

--- src/test/java/eu/debooy/natuurtools/IocNamenTest.java
+++ src/test/java/eu/debooy/natuurtools/IocNamenTest.java
@@ -16,11 +16,18 @@
  */
 package eu.debooy.natuurtools;
 
+import eu.debooy.doosutils.access.Bestand;
+import eu.debooy.doosutils.exception.BestandException;
 import eu.debooy.doosutils.test.BatchTest;
+import eu.debooy.doosutils.test.DoosUtilsTestConstants;
 import eu.debooy.doosutils.test.VangOutEnErr;
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -32,11 +39,52 @@ public class IocNamenTest extends BatchTest {
   protected static final  ClassLoader CLASSLOADER =
       IocNamenTest.class.getClassLoader();
 
+  private static final  String  BST_CSV   = "MultilingIOC.csv";
+  private static final  String  BST_JSON  = "MultilingIOC.json";
+
+  @AfterClass
+  public static void afterClass() {
+//    verwijderBestanden(TEMP + File.separator,
+//                       new String[] {BST_CSV, BST_JSON});
+  }
+
   @BeforeClass
-  public static void beforeClass() {
+  public static void beforeClass() throws BestandException {
     Locale.setDefault(new Locale("nl"));
     resourceBundle  = ResourceBundle.getBundle("ApplicatieResources",
                                                Locale.getDefault());
+
+    try {
+      kopieerBestand(CLASSLOADER, BST_CSV,
+                     TEMP + File.separator + BST_CSV);
+    } catch (IOException e) {
+      System.out.println(e.getLocalizedMessage());
+      throw new BestandException(e);
+    }
+  }
+
+  @Test
+  public void testCsv() throws BestandException {
+    String[]  args      = new String[] {
+      "--" + NatuurTools.PAR_IOCBESTAND + "=" + BST_CSV,
+      "--" + NatuurTools.PAR_TALEN + "=en,af,ca,zh,z,hr,cs,da,nl,et,fi,fr,de,hu,is,id,it,ja,lv,lt,se,no,pl,pt,ru,sk,sl,es,sv,th,uk",
+      "--invoerdir=" + TEMP};
+
+    VangOutEnErr.execute(IocNamen.class,
+                         DoosUtilsTestConstants.CMD_EXECUTE, args, out, err);
+
+    assertEquals("CSV - helptekst", 18, out.size());
+    assertEquals("CSV - fouten", 0, err.size());
+    assertEquals("CSV - 12", "33", out.get(11).split(":")[1].trim());
+    assertEquals("CSV - 13", "2",  out.get(12).split(":")[1].trim());
+    assertEquals("CSV - 14", "3",  out.get(13).split(":")[1].trim());
+    assertEquals("CSV - 15", "6",  out.get(14).split(":")[1].trim());
+    assertTrue("CSV - equals",
+        Bestand.equals(
+            Bestand.openInvoerBestand(TEMP + File.separator
+                                      + BST_JSON),
+            Bestand.openInvoerBestand(IocNamenTest.class.getClassLoader(),
+                                      BST_JSON)));
   }
 
   @Test
