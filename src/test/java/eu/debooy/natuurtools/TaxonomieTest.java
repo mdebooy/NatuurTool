385d5c1ec5472e6e412705b579b8abc2ef9256ea
Marco DE BOOIJ <marco.developer@debooy.eu>
2021-09-29 19:42:35 +0200

    jUnit voor Taxonomie.

---
+++ src/test/java/eu/debooy/natuurtools/TaxonomieTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021 Marco de Booij
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * you may not use this work except in compliance with the Licence. You may
+ * obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+package eu.debooy.natuurtools;
+
+import eu.debooy.doosutils.test.BatchTest;
+import eu.debooy.doosutils.test.VangOutEnErr;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Marco de Booij
+ */
+public class TaxonomieTest extends BatchTest {
+  protected static final  ClassLoader CLASSLOADER =
+      TaxonomieTest.class.getClassLoader();
+
+  @BeforeClass
+  public static void beforeClass() {
+    Locale.setDefault(new Locale("nl"));
+    resourceBundle  = ResourceBundle.getBundle("ApplicatieResources",
+                                               Locale.getDefault());
+  }
+
+  @Test
+  public void testLeeg() {
+    var args  = new String[] {};
+
+    VangOutEnErr.execute(Taxonomie.class, "execute", args, out, err);
+
+    assertEquals("Zonder parameters - helptekst", 29, out.size());
+    assertEquals("Zonder parameters - fouten", 1, err.size());
+  }
+}
